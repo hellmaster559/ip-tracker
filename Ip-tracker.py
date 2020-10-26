f224576a05a03413d357b100c90b694bd71c3f59
hellmaster559 <38070088+hellmaster559@users.noreply.github.com>
2020-10-27 00:04:22 +0530

    Add files via upload

---
+++ Ip-tracker.py
@@ -0,0 +1,11 @@
+import geoip2.database
+reader = geoip2.database.Reader(r'C:\Users\hellm\Desktop\New folder\GeoLite2-City_20201020\GeoLite2-City_20201020\GeoLite2-City.mmdb')
+i = input("Input The IP: ")
+response = reader.city(i)
+print("County Code is", response.country.iso_code)
+print("Country is",response.country.name)
+print("Postal Code is",response.postal.code)
+print("Subdivision is",response.subdivisions.most_specific.name)
+print("City is",response.city.name)
+print("Latitude is",response.location.latitude)
+print("Longitude is",response.location.longitude)
